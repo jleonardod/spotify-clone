d536d180d3d1e209d44b79470dec7a443ccb6aa3
Leonardo Diaz <jldiaz9623@gmail.com>
2023-08-13 13:55:24 -0500

    Add function to modal close and open

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -7,15 +7,29 @@ import { useRouter } from "next/navigation"
 import { Auth } from "@supabase/auth-ui-react"
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 import useAuthModal from "@/hooks/UseAuthModal"
+import { useEffect } from "react"
 
 const AuthModal = () => {
   const supabaseClient = useSupabaseClient()
-  const router = useRouter
+  const router = useRouter()
   const { session } = useSessionContext()
   const { onClose, isOpen } = useAuthModal()
 
+  useEffect(() => {
+    if (session){
+      router.refresh()
+      onClose()
+    }
+  }, [session, router, onClose])
+
+  const onChange = (open: boolean) => {
+    if(!open) {
+      onClose()
+    }
+  }
+
   return(
-    <Modal title="Welcome back" description="Login to your account" isOpen={isOpen} onChange={() => {}}>
+    <Modal title="Welcome back" description="Login to your account" isOpen={isOpen} onChange={onChange}>
       <Auth 
         theme="dark" 
         magicLink 
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { twMerge } from "tailwind-merge"
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi"
+import useAuthModal from "@/hooks/UseAuthModal"
 
 interface HeaderProps {
   children: React.ReactNode
@@ -14,6 +15,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({children, className}) => {
+  const authModal = useAuthModal()
   const router = useRouter()
 
   const handleLogout = () => {
@@ -39,10 +41,10 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">Sign up</Button>
+              <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">Sign up</Button>
             </div>
             <div>
-              <Button onClick={() => {}} className="bg-white px-6 py-2">Log in</Button>
+              <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">Log in</Button>
             </div>
           </>
         </div>
