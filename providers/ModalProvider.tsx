39de3c23c68de3e18c41a8e245d90cad6f56d171
Leonardo Diaz <jldiaz9623@gmail.com>
2023-08-13 13:38:27 -0500

    Add design of login modal

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import Modal from "@/components/Modal"
-
 import { useEffect, useState } from "react"
+
+import AuthModal from "@/components/AuthModal"
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -17,9 +17,7 @@ const ModalProvider = () => {
 
   return (
     <>
-      <Modal title="Test Modal" description="Test Description" isOpen onChange={() => {}}>
-        Test Children
-      </Modal>
+      <AuthModal />
     </>
   )
 }
