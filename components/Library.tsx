827233404593f47f4355951f730f367f23c951e5
Leonardo Diaz <jldiaz9623@gmail.com>
2023-08-13 19:31:21 -0500

    Add modal upload songs and functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,10 +1,24 @@
 "use client"
+
 import { TbPlaylist } from "react-icons/tb"
 import { AiOutlinePlus } from "react-icons/ai"
+import useAuthModal from "@/hooks/UseAuthModal"
+import { useUser } from "@/hooks/useUser"
+import useUploadModal from "@/hooks/useUploadModal"
 
 const Library = () => {
-  const onClick = () => {
+  const authModal = useAuthModal()
+  const uploadModal = useUploadModal()
+  const { user } = useUser()
 
+  const onClick = () => {
+    if(!user) {
+      return authModal.onOpen()
+    }
+
+    // TODO: Check for subscription
+
+    return uploadModal.onOpen()
   }
   return(
     <div className="flex flex-col">
